d0208bb3f17c315424371913932bcb3387e72610
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-06-23 16:14:02 +0530

    first page initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>Hello, This is my portfolio website !</h1>
-          <h1>this is going to be my first page, which means the home page</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+     <Navbar/>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./Navbar.css"
+
+const Navbar = () => {
+    return(
+        <div>
+            <h1>This is navbar</h1>
+        </div>
+    )
+}
+
+export default Navbar
