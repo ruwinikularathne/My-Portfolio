a21a754e6a5f555c81effd8134cce387522143bd
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-06-30 02:15:07 +0530

    added all home page content

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import "./Header.css"
 import {profile1} from "../../images";
+import { FaGithub, FaLinkedin, FaInstagram } from 'react-icons/fa'; 
+import Typewriter from 'typewriter-effect';
 
 const Header = () => {
     return(
@@ -13,8 +15,26 @@ const Header = () => {
                             Hello! I'm <span className='color_primary'>Ruwini Kularathne</span>
                         </div>                           
                          <div className="header_info_middle">
-                              <h1 className='primary_title header_title'>I'm a Web Developer</h1>
+                              <h1 className='primary_title header_title'>I'm a <Typewriter
+                                    options={{
+                                        strings: ['Web Developer', 'UI/UX Designer'],
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </h1>
                         </div>
+                        <div className="social-icons">
+                                <a href="https://github.com/ruwinikularathne" target="_blank" rel="noopener noreferrer">
+                                    <FaGithub size={30} />
+                                </a>
+                                <a href="https://www.linkedin.com/in/ruwini-kularathne-13642a21b/?original_referer=https%3A%2F%2Fwww%2Egoogle%2Ecom%2F&originalSubdomain=lk" target="_blank" rel="noopener noreferrer">
+                                    <FaLinkedin size={30} />
+                                </a>
+                                <a href="https://www.instagram.com/ruwini_kularathne/?igsh=MWtyN2hwemxvOXk5bQ%3D%3D" target="_blank" rel="noopener noreferrer">
+                                    <FaInstagram size={30} />
+                                </a>
+                            </div>
                         <div className="header_info_bottom">
                             <button className='btn'>Download CV</button>
                             <button className='btn'>Contact Me</button>
