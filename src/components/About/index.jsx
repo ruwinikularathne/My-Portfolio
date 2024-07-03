c10824b7a4fda5708043bb23d3e31bfd59cae391
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-07-03 22:09:01 +0530

    updated about page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,30 +1,43 @@
-import React from 'react'
-import "./About.css"
-import {profile2} from "../../images";
+import React from 'react';
+import './About.css';
+import { profile2 } from '../../images';
 
 const About = () => {
-    return(
-        
+    return (
         <section id='about'>
             <div className="section_wrapper about_container">
                 <div className="me_container blur-effect">
                     <div className="photo_container">
-                        <img src={profile2} alt=""/>
+                        <img src={profile2} alt="" />
                     </div>
                 </div>
                 <div className="section_header">
-                 <h2 className="primary_title">About Me</h2>
-                 <h1 className="title">I am <span className="color_primary">Ruwini Kularathne</span></h1>
-                 <p className="text_muted description">
-                 I am an aspiring Web Developer passionate about continuous learning and growth with 
-                strong organizational and communication skills. My ability to adapt and collaborate 
-                effectively with teams makes me a reliable and proactive contributor. I am passionate 
-                about pursuing opportunities in the IT industry to further enhance my skills and knowledge. 
-                 </p>
+                    <h2 className="primary_title">About Me</h2>
+                    <h1 className="title">I am <span className="color_primary">Ruwini Kularathne</span></h1>
+                    <p className="text_muted description">
+                        I am an aspiring Web Developer passionate about continuous learning and growth with 
+                        strong organizational and communication skills. My ability to adapt and collaborate 
+                        effectively with teams makes me a reliable and proactive contributor. I am passionate 
+                        about pursuing opportunities in the IT industry to further enhance my skills and knowledge.
+                    </p>
+                    <div className="metrics_container">
+                        <div className="metric">
+                            <h3>10+</h3>
+                            <p>Completed Projects</p>
+                        </div>
+                        <div className="metric">
+                            <h3>8+</h3>
+                            <p>Volunteering Experiences</p>
+                        </div>
+                        <div className="metric">
+                            <h3>5+</h3>
+                            <p>Achievements</p>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
-    )
+    );
 }
 
-export default About
+export default About;
