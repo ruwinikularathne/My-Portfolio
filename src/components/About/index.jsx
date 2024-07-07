29541458e9f82836c5eb4bd93dbc49c1f1db4eac
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-07-08 01:42:33 +0530

    updated about me page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -13,7 +13,6 @@ const About = () => {
                 </div>
                 <div className="section_header">
                     <h2 className="primary_title">About Me</h2>
-                    <h1 className="title">I am <span className="color_primary">Ruwini Kularathne</span></h1>
                     <p className="text_muted description">
                     I am an aspiring Web Developer passionate about continuous learning and growth. 
                     My ability to adapt and collaborate effectively with teams makes me a reliable 
@@ -22,15 +21,15 @@ const About = () => {
                     </p>
                     <div className="metrics_container">
                         <div className="metric">
-                            <h3>10+</h3>
+                            <h3>10</h3>
                             <p>Completed Projects</p>
                         </div>
                         <div className="metric">
-                            <h3>8+</h3>
+                            <h3>8</h3>
                             <p>Volunteering Experiences</p>
                         </div>
                         <div className="metric">
-                            <h3>5+</h3>
+                            <h3>5</h3>
                             <p>Achievements</p>
                         </div>
                     </div>
