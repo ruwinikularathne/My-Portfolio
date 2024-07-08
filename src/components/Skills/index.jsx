7eb5eb2a18458e0c7b69fe707a914d52c8aa3d0d
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-07-08 19:18:47 +0530

    Skills page done

--- src/components/Skills/index.jsx
+++ src/components/Skills/index.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import "./Skills.css"
+import React from 'react';
+import "./Skills.css";
+import { FaReact, FaCss3Alt, FaJs, FaDatabase } from 'react-icons/fa';
+import { SiFlutter, SiTailwindcss, SiNextdotjs, SiFirebase } from 'react-icons/si';
 
 const Skills = () => {
-    return(
-        <section id='about'>
-            
-        </section>
-    )
-}
+    const skills = [
+        { icon: <SiFlutter size={80} />, name: 'Flutter' },
+        { icon: <FaReact size={80} />, name: 'React' },
+        { icon: <SiTailwindcss size={80} />, name: 'Tailwind' },
+        { icon: <FaCss3Alt size={80} />, name: 'CSS' },
+        { icon: <SiNextdotjs size={80} />, name: 'Next.js' },
+        { icon: <FaJs size={80} />, name: 'JavaScript' },
+        { icon: <SiFirebase size={80} />, name: 'Firebase' },
+        { icon: <FaDatabase size={80} />, name: 'MySQL' }
+    ];
 
-export default Skills
+    return (
+        <section id='skills'>
+            <div className="section_wrapper skills_container">         
+            <h2 className="primary_title">Skills</h2>
+                <div className="skills_grid">
+                    {skills.map((skill, index) => (
+                        <div key={index} className="skill_card">
+                            {skill.icon}
+                            <p>{skill.name}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Skills;
