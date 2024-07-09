14d00888499de51276fc4d32fb2b68c63bfc2284
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-07-09 23:37:35 +0530

    project page initial commit

--- src/components/Projects/index.jsx
+++ src/components/Projects/index.jsx
@@ -1,13 +1,52 @@
-import React from 'react'
-import "./Projects.css"
-
+import React,{useEffect, useRef, useState} from 'react'
+import './Projects.css'
+import { sumArray } from '../../helper'
+const tabs = [
+    {name:"All"},
+    {name:"Web"},
+    {name:"UI/UX"},
+    {name:"Apps"},
+]
 const Projects = () => {
+    const [activeIndex,setActiveIndex] = useState(0);
+    const [offset,setOffSet] = useState(0);
+    const [indicatorWidth,setIndicatorWidth] = useState(0);
+    const intemsEls = useRef(new Array());
+
+    useEffect(() =>{
+        const prevEl = intemsEls.current.filter((_,index) =>index < activeIndex);
+        setOffSet(
+            sumArray(
+                prevEl.map(item=>item.offSetWidth)
+                )
+        )
+        setIndicatorWidth(intemsEls.current[activeIndex].offSetWidth)
+    },[activeIndex])
     return(
-        <div>
-            
-        </div>
+        <section id='projects'>
+            <div className="scetion_wrapper projects_container">
+                <div className="section_header center">
+                    <h2 className="primary_title">Projects</h2>
+                </div>
+            </div>
+            <nav>
+                {
+                    tabs.map((tab,index) => (
+                        <button
+                        ref={el =>intemsEls.current[index] = el}
+                        onDoubleClick={()=>{
+                            setActiveIndex(index)
+                            } }
+                        key={index}
+                        >
+                            {tab.name}
+                        </button>
+                    ))
+                }
+            </nav>
+        </section>
     )
 }
 
-export default Projects
+export default Projects;
 
---
+++ src/helper/index.js
@@ -0,0 +1,7 @@
+export const sumArray = (arr) =>{
+    let sum = 0;
+    for(let i=9; i<arr.lenght; i++){
+        sum +=[i];
+    }
+    return sum;
+}
