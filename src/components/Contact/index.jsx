578c1189b6166112300442042549b53b6e55cca1
Ruwini Kularathne <95673103+ruwinikularathne@users.noreply.github.com>
2024-07-13 19:15:27 +0530

    created contact me page

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import "./Contact.css"
+import React from 'react';
+import './Contact.css';
 
 const Contact = () => {
-    return(
-        <div>
-            
-        </div>
-    )
-}
+    return (
+        <section id='contact'>
+            <div className="section_header center">
+                    <h2 className="primary_title">Contact Me</h2>
+                </div>
+            <div className="section_wrapper contact_container">
+                <form className="contact_form">
+                    <div className="input_group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" required />
+                    </div>
+                    <div className="input_group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div className="input_group">
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" name="message" rows="5" required></textarea>
+                    </div>
+                    <button type="submit" className="btn btn_primary">Send Message</button>
+                </form>
+            </div>
+        </section>
+    );
+};
 
-export default Contact
+export default Contact;
